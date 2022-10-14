e5bc809e56244b08e14d2c4aeb2ab67458c82a0e
Daxesh Solanki <87466510+SolankiDaxesh@users.noreply.github.com>
2022-10-14 11:31:32 +0000

    News API key

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -5,11 +5,22 @@ export class News extends Component {
   static PorpTypes = {}
   render() {
     return (
-      <>
-        <p>News Component</p>
-
-        <NewsItem />
-      </>
+      <div className="container my-3">
+        <h2>Top Headlines</h2>
+        <div className="row">
+          {this.State.articles.map((element) => {
+            return <div className="col-md-4" key={element.url}>
+              <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage}></NewsItem>
+            </div>
+          })}
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="description" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="myTitle" description="description" />
+          </div>
+        </div>
+      </div >
     )
   }
 }
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -3,10 +3,27 @@ import PropTypes from "prop-types"
 
 export class NewsItem extends Component {
   static PorpTypes = {}
+  constructor() {
+    super()
+    console.log("Constructor Say's Hello!!")
+    this.state = {
+      articles: this.articles,
+      loading: false
+    }
+  }
+
   render() {
+    let { title, description, newsUrl } = this.props;
     return (
       <>
-        <p>NewsItem Component</p>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src="./public/download.jpg" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a /*href={newsUrl}*/ target="_blank" className="btn btn-primary">Read More</a>
+          </div>
+        </div>
       </>
     )
   }
